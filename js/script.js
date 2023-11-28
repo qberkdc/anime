1be608d4d51735b8bc87f7f73f9be858643f4c64
Berk <85903065+qberkdc@users.noreply.github.com>
2023-11-29 01:14:13 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,16 +23,17 @@ $(window).on('load', function()
 	 	for(let i = 0; i <ca.length; i++) 
 		{
  			let c = ca[i];
-	    	while (c.charAt(0) == ' ') 
+	    		while (c.charAt(0) == ' ') 
 			{
 				c = c.substring(1);
-	    	}
+	    		}
 	
 			if (c.indexOf(name) == 0)
 			{
-	    		return c.substring(name.length, c.length);
-	  	  }
+	    			return c.substring(name.length, c.length);
+	  	  	}
 		}
+		
 		return 0;
 	}
 
